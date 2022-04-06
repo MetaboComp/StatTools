7b5be1d004ca80cf8b5f4321a237c04fd66f1dce
Yingxiao Yan <yingxiao@net.chalmers.se>
2022-04-06 16:52:12 +0200

    fix bug

--- R/checkforsanity.R
+++ R/checkforsanity.R
@@ -12,34 +12,55 @@ checkforsanity<-function(data,
   if(missing(output)){output="number"}
   if(missing(check)){check="NA"}
   if(check=="NA"){
-    if(output=="number"){result=table(apply(data,2,function(x){sum(is.na(x))}))}
+    if(output=="number"){result=table(apply(data,2,function(x){sum(is.na(x))}))
+    cat("The first row is how many values is NA in individual columns.")
+    cat("The second row is how many percentage individual columns have such values")
+    }
     if(output=="percentage"){result=table(apply(data,2,function(x){sum(is.na(x))/nrow(data)}))
     cat("The first row is how many values is NA in individual columns.")
-    cat("The second row is how many such individual columns have such values")}
+    cat("The second row is how many such individual columns  have such values")
+    }
   }
   if(check=="NaN"){
-    if(output=="number"){result=table(apply(data,2,function(x){sum(is.nan(x))}))}
+    if(output=="number"){result=table(apply(data,2,function(x){sum(is.nan(x))}))
+    cat("The first row is how many values is NaN in individual columns.")
+    cat("The second row is how many percentage  individual columns have such values")
+    
+    }
     if(output=="percentage"){result=table(apply(data,2,function(x){sum(is.nan(x))/nrow(data)}))
     cat("The first row is how many values is NaN in individual columns.")
-    cat("The second row is how many such individual columns have such values")}
+    cat("The second row is how many percentage individual columns have such values")
+    }
   }
   if(check=="negative"){
-    if(output=="number"){result=table(apply(data,2,function(x){sum(x<0)}))}
+    if(output=="number"){result=table(apply(data,2,function(x){sum(x<0)}))
+    cat("The first row is how many values is negative in individual columns.")
+    cat("The second row is how many such individual columns have such values")
+    }
     if(output=="percentage"){result=table(apply(data,2,function(x){sum(x<0)/nrow(data)}))
     cat("The first row is how many values is negative in individual columns.")
-    cat("The second row is how many such individual columns have such values")}
+    cat("The second row is how many such individual columns have such values")
+    }
   }
   if(check=="0"){
-    if(output=="number"){result=table(apply(data,2,function(x){sum(x==0)}))}
+    if(output=="number"){result=table(apply(data,2,function(x){sum(x==0)}))
+    cat("The first row is how many values is 0 in individual columns.")
+    cat("The second row is how many such individual columns have such values")
+    }
     if(output=="percentage"){result=table(apply(data,2,function(x){sum(x==0)/nrow(data)}))
     cat("The first row is how many values is 0 in individual columns.")
-    cat("The second row is how many such individual columns have such values")}
+    cat("The second row is how percentage such individual columns have such values")
+    }
   }
   if(check=="infinite"){
-    if(output=="number"){result=table(apply(data,2,function(x){sum(is.infinite(x))}))}
+    if(output=="number"){result=table(apply(data,2,function(x){sum(is.infinite(x))}))
+    cat("The first row is how many values is infinite in individual columns.")
+    cat("The second row is how many such individual columns have such values")
+    }
     if(output=="percentage"){result=table(apply(data,2,function(x){sum(is.infinite(x))/nrow(data)}))
     cat("The first row is how many values is infinite in individual columns.")
-    cat("The second row is how many such individual columns have such values")}
+    cat("The second row is how many percentage individual columns have such values")
+    }
   }
   return(result)
 }
