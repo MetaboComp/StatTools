a166cd0772112c9c8a92a64989539e42083fe86a
Yingxiao Yan <yingxiao@net.chalmers.se>
2022-05-11 11:49:41 +0200

    summary _stuff

---
+++ R/summary_stuff.R
@@ -0,0 +1,14 @@
+#' Summary stuff
+#' @param V variable for all observations
+#' @param type  mean, standard deviation,variance, standard error or table
+#' @param ID unique ID number is smaller than number of observations
+
+#' @return summary statistics
+#' @export
+
+summary_stuff<-function(V,
+                        type=c("mean","sd","variance","table"),
+                        ID
+){
+  
+}
